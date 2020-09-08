1e1ac9d98fb3e3b16aa721c079ef9c73f91b5ff3
Jon-Ting <jonting97@gmail.com>
2020-09-08 16:41:45 +0800

    Regenerated .parquet files without compression

--- Credit_Card_Approval_Prediction_Data/csv_to_parquet.py
+++ Credit_Card_Approval_Prediction_Data/csv_to_parquet.py
@@ -2,5 +2,5 @@ import pandas as pd
 
 df1 = pd.read_csv("application_record.csv")
 df2 = pd.read_csv("credit_record.csv")
-df1.to_parquet(path="application_record.parquet")
-df2.to_parquet(path="credit_record.parquet")
+df1.to_parquet(path="application_record.parquet", compression=None)
+df2.to_parquet(path="credit_record.parquet", compression=None)
